9013f393f5688d66d52a151fbc83a65908f8c5f6
Luis Adorno <luis.adorno5@upr.edu>
2022-07-13 18:03:41 -0400

    Initial commit.

---
+++ server/employee-manager-server/src/main/java/com/luisadorno/employeemanagerserver/EmployeeManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.luisadorno.employeemanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerServerApplication.class, args);
+	}
+
+}
